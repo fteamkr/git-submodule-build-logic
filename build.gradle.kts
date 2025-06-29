8073050e4de25c8172d9f65b1f5d40f5817b5ca3
Jaisung Lee <jaisung.lee@fteam.kr>
2025-06-29 19:29:26 +0900

    fix kotlin-dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-  id("kotlin-dsl")
+  `kotlin-dsl`
 }
 
 repositories {
+  gradlePluginPortal()
+
   mavenCentral()
 }
 
 dependencies {
-  implementation 'org.springframework.boot:spring-boot-gradle-plugin:2.7.18'
+  implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.18")
 }
